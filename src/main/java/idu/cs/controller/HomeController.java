5c02f7652787bf3badd156cc64f844fa8c97a1b8
comso5 <induk@naver.com>
2019-05-15 16:09:52 +0900

    회원 등록까지 가능

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -18,14 +18,21 @@ public class HomeController {
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("test", "인덕 컴소");
-		model.addAttribute("egy", "유응구");
+		model.addAttribute("ksh", "고성환");
 		return "index";
 	}
+	
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
 		model.addAttribute("users", userRepo.findAll());
 		return "userlist";
 	}
+	
+	@GetMapping("/register") // 추가한거
+	public String getRegForm(Model model) {
+		return "form";
+	}
+	
 	@PostMapping("/users")
 	public String createUser(@Valid @RequestBody User user, Model model) {
 		userRepo.save(user);
